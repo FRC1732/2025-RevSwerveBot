e480db013056a10147de7666145e30d5f9ddffc9
Alan Bork <aebork@gmail.com>
2025-01-08 12:56:07 -0600

    Set CAN IDs for the drive train.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -41,17 +41,17 @@ public class DriveConstants {
   public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
 
   // Device CAN IDs
-  public static final int pigeonCanId = 9;
+  public static final int pigeonCanId = 55;
 
-  public static final int frontLeftDriveCanId = 1;
-  public static final int backLeftDriveCanId = 3;
-  public static final int frontRightDriveCanId = 5;
-  public static final int backRightDriveCanId = 7;
+  public static final int frontLeftDriveCanId = 30;
+  public static final int backLeftDriveCanId = 10;
+  public static final int frontRightDriveCanId = 40;
+  public static final int backRightDriveCanId = 20;
 
-  public static final int frontLeftTurnCanId = 2;
-  public static final int backLeftTurnCanId = 4;
-  public static final int frontRightTurnCanId = 6;
-  public static final int backRightTurnCanId = 8;
+  public static final int frontLeftTurnCanId = 31;
+  public static final int backLeftTurnCanId = 11;
+  public static final int frontRightTurnCanId = 41;
+  public static final int backRightTurnCanId = 21;
 
   // Drive motor configuration
   public static final int driveMotorCurrentLimit = 50;
